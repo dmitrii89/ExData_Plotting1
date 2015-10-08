f4ecf32033f3d1707a621f9a63427528b3b3088a
Dmitrii <dmitry_cher@amil.ru>
2015-10-08 17:52:49 +0300

    updated comments to second plot script

--- plot2.R
+++ plot2.R
@@ -6,11 +6,13 @@ period <- c("1/2/2007","2/2/2007")
 data <- file[file$Date %in% period,]
 # getting data as numeric only about global active power
 globalActivePower <- as.numeric(data$Global_active_power)
+# connecting date and time fields
 datetime <- paste(data$Date, data$Time, sep = " ")
+# making date and time according to format
 formdate <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 # making png graphics device
 png("plot2.png", width=480, height=480)
-# drawing histogram according to task
+# drawing plot according to task
 plot(formdate, globalActivePower, type = "l" , xlab ='', ylab="Global Active Power (kilowatts)")
 # shut down device
 dev.off()
